278903e74ce11b6b7784d7d335b51376b36b53d4
tsasaki22256 <tsasaki128@gmail.com>
2021-01-23 23:47:37 +0900

    edit test

--- test/plugins/opdt.spec.js
+++ test/plugins/opdt.spec.js
@@ -13,6 +13,7 @@ describe('plugins/opdt', () => {
 
     const testData = {
       departure: {
+        "dc:date":"2021-01-15T16:10:48+09:00",
         "owl:sameAs": "odpt.FlightInformationDeparture:JAL.HND.JL163",
         "odpt:airline": "odpt.Operator:JAL",
         "odpt:operator": "odpt.Operator:JAL",
@@ -24,6 +25,7 @@ describe('plugins/opdt', () => {
         "odpt:scheduledDepartureTime": "10:55"
       },
       arrival: {
+        "dc:date":"2021-01-15T16:13:44+09:00",
         "owl:sameAs": "odpt.FlightInformationArrival:JAL.AXT.JL163",
         "odpt:airline": "odpt.Operator:JAL",
         "odpt:operator": "odpt.Operator:JAL",
@@ -35,9 +37,22 @@ describe('plugins/opdt', () => {
         "odpt:scheduledArrivalTime": "12:05"
       }
     };
+
     const testParsed = parseFlightDataJsonElement(testData.departure, testData.arrival);
+    expect(testParsed.airline).toBe('JAL');
     expect(testParsed.number).toBe('163');
+    expect(testParsed.aircraftType).toBe('73H');
+    expect(testParsed.origin).toBe('羽田');
+    expect(testParsed.actualArrivalTime).toBe('11:46');
+    expect(testParsed.isEstimatedArrivalTime).toBeFalsy();
+    expect(testParsed.scheduledArrivalTime).toBe('12:05');
+    expect(testParsed.actualDepartureTime).toBe('10:47');
+    expect(testParsed.isEstimatedDepartureTime).toBeFalsy();
+    expect(testParsed.scheduledDepartureTime).toBe('10:55');
+    expect(testParsed.date).toBe('2021-01-15T16:13:44+09:00');
     expect(testParsed.status).toBe('到着済み');
     expect(testParsed.cancelled).toBeFalsy();
+    expect(testParsed.arrivalDelay).toBe(-19);
+    expect(testParsed.departureDelay).toBe(-8);
   });
 });
